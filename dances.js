a9b0b99d980cdaf07d3340fee356439b6d332f84
Jeff Kaufman <jeff.t.kaufman@gmail.com>
2022-05-02 21:31:33 -0400

    import

---
+++ dances.js
@@ -0,0 +1,591 @@
+// Format:
+//   [year, month, day]
+//   event title
+//   event page link
+//   optional html to inject into event body
+//
+// You don't need to remove past events; they are automatically hidden
+// with JS.
+e = [
+  [[2016, 10, 16],
+   "Contra + Family Dance",
+   "https://www.facebook.com/events/1798408000395535/",
+   "<table>\
+      <tr><td>5:30-6:30:<td>family dance + open band\
+      <tr><td>6:30-7:30:<td>potluck dinner\
+      <tr><td>7:30-10:30:&nbsp;&nbsp;<td>contra dance\
+    </table>"],
+  [[2016, 11, 4],
+   "Cancelled: Tunes Jam",
+   "https://www.facebook.com/events/123715564767129/"],
+  [[2016, 11, 6],
+   "Contra + French Dance",
+   "https://www.facebook.com/events/181402685648008/"],
+  [[2016, 11, 20],
+   "Contra Dance",
+   "https://www.facebook.com/events/346343689032967/"],
+  [[2016, 12, 4],
+   "Contra Dance + Open Band",
+   "https://www.facebook.com/events/1806816232868181/"],
+  [[2016, 12, 9],
+   "Tunes Jam",
+   "https://www.facebook.com/events/1287519351307009/"],
+  [[2016, 12, 18],
+   "Contra + Family Dance",
+   "https://www.facebook.com/events/1740016379583446/",
+    "<table>\
+      <tr><td>5:30-6:30:<td>family dance + open band\
+      <tr><td>6:30-7:30:<td>potluck dinner\
+      <tr><td>7:30-10:30:&nbsp;&nbsp;<td>contra dance\
+    </table>"],
+  [[2017, 1, 1],
+   "Contra Dance",
+   "https://www.facebook.com/events/255786891503916/",
+   "<i>Note: different time and place</i>\
+    <p>6:30-9:30 at the Somerville Armory"],
+  [[2017, 1, 13],
+   "Cancelled: Tunes Jam",
+   "https://www.facebook.com/events/1313002352085987/"],
+  [[2017, 1, 15],
+   "Contra + Family Dance",
+   "https://www.facebook.com/events/148208402326176/",
+   "<table>\
+      <tr><td>5:30-6:30:<td>family dance + open band\
+      <tr><td>6:30-7:30:<td>potluck dinner\
+      <tr><td>7:30-10:30:&nbsp;&nbsp;<td>contra dance\
+    </table>"],
+  [[2017, 2, 5],
+   "Contra Dance",
+   "https://www.facebook.com/events/831361257003881/"],
+  [[2017, 2, 19],
+   "Contra Dance",
+   "https://www.facebook.com/events/426059384451810/"],
+  [[2017, 3, 5],
+   "Contra Dance",
+   "https://www.facebook.com/events/1609587239062879/"],
+  [[2017, 3, 19],
+   "Contra + Chestnuts Dance",
+   "https://www.facebook.com/events/1371900869527382/",
+   "A 'chestnut' in contra dancing\
+   is a much beloved older dance, often with a particular musical\
+   pairing&mdash;and we're going to mix some of these great classics into\
+   your regularly scheduled programming." ],
+  [[2017, 4, 2],
+   "Contra + Family Dance + Annual Meeting",
+   "https://www.facebook.com/events/1631926847113243/"],
+  [[2017, 4, 16],
+   "Contra Dance",
+   "https://www.facebook.com/events/441175372893957/"],
+  [[2017, 5, 7],
+   "Contra Dance",
+   "https://www.facebook.com/events/688848671298246/",
+  ],
+  [[2017, 5, 21],
+   "Contra Dance + Open Band",
+   "https://www.facebook.com/events/1433770406643381/",
+  ],
+  [[2017, 6, 4],
+   "Contra Dance",
+   "https://www.facebook.com/events/1474332579253848/",
+  ],
+  [[2017, 6, 18],
+   "Contra + Family Dance",
+   "https://www.facebook.com/events/433674550321262/",
+  ],
+  [[2017, 6, 23],
+   "Spark in the Dark",
+   "https://www.facebook.com/events/654557714752091/",
+  ],
+  [[2017, 7, 2],
+   "Contra Dance",
+   "https://www.facebook.com/events/1790822677914717/",
+  ],
+  [[2017, 7, 16],
+   "Contra + Family Dance at the Somerville Armory",
+   "https://www.facebook.com/events/2021926354695006/",
+  ],
+  [[2017, 8, 6],
+   "Contra Dance at the Somerville Armory",
+   "https://www.facebook.com/events/1105120249621663/",
+  ],
+  [[2017, 8, 11],
+   "Tunes Jam",
+   "https://www.facebook.com/events/137742760155377/"],
+  [[2017, 8, 20],
+   "Contra + Family Dance at the Somerville Armory",
+   "https://www.facebook.com/events/118059022142416",
+  ],
+  [[2017, 9, 3],
+   "Contra Dance, back at the Cambridge Masonic Hall",
+   "https://www.facebook.com/events/983112241831661/",
+  ],
+  [[2017, 9, 17],
+   "*Free* Contra Dance + Family Dance",
+   "https://www.facebook.com/events/1952062901712111/",
+  ],
+  [[2017, 10, 1],
+   "Contra Dance",
+   "https://www.facebook.com/events/619533791768595/",
+  ],
+  [[2017, 10, 11],
+   "Open Jam",
+   "https://www.facebook.com/events/1789481778017946/",
+  ],
+  [[2017, 10, 14],
+   "Spark in the Dark",
+   "https://www.facebook.com/events/172577149957582/"
+  ],
+  [[2017, 10, 15],
+   "Contra Dance + Open Band",
+   "https://www.facebook.com/events/140875713196510/",
+  ],
+  [[2017, 11, 5],
+   "Contra + Family Dance",
+   "https://www.facebook.com/events/421968388205184/",
+  ],
+  [[2017, 11, 15],
+   "Bandleading Tunes Party",
+   "https://www.facebook.com/events/171598826757785/",
+  ],
+  [[2017, 11, 19],
+   "Contra Dance",
+   "https://www.facebook.com/events/1796605573745567/",
+  ],
+  [[2017, 12, 3],
+   "Contra Dance + Open Band + Open Bandleading",
+   "https://www.facebook.com/events/137094043715901/",
+  ],
+  [[2017, 12, 17],
+   "Contra Dance",
+   "https://www.facebook.com/events/1553620488053626/",
+  ],
+  [[2018, 1, 7],
+   "Contra Dance",
+   "https://www.facebook.com/events/126007021502286/",
+  ],
+  [[2018, 1, 19],
+   "Spark in the Dark",
+   "https://www.facebook.com/events/1705892206111550/",
+  ],
+  [[2018, 1, 21],
+   "Musician Workshop",
+   "https://www.facebook.com/events/117588775603713/",
+  ],
+  [[2018, 1, 21],
+   "Contra + Family Dance",
+   "https://www.facebook.com/events/181576835764899/",
+   "<table>\
+      <tr><td>5:30-6:30:<td>family dance + open band\
+      <tr><td>6:30-7:30:<td>potluck dinner\
+      <tr><td>7:30-10:30:&nbsp;&nbsp;<td>contra dance\
+    </table>"],
+  [[2018, 2, 4],
+   "Contra Dance",
+   "https://www.facebook.com/events/216329382268471/",
+  ],
+  [[2018, 2, 18],
+   "Contra Dance",
+   "https://www.facebook.com/events/137036173774460/",
+  ],
+  [[2018, 2, 28],
+   "Open Jam",
+   "https://www.facebook.com/events/1907473139565268/",
+  ],
+  [[2018, 3, 4],
+   "Contra Dance",
+   "https://www.facebook.com/events/1190093874460046/",
+  ],
+  [[2018, 3, 18],
+   "Contra Dance",
+   "https://www.facebook.com/events/358601874635881/",
+  ],
+  [[2018, 4, 1],
+   "Contra + Family Dance",
+   "https://www.facebook.com/events/357125214785936/"
+  ],
+  [[2018, 4, 4],
+   "Introductory Sound Workshop",
+   "https://www.facebook.com/events/1106142452859553/",
+  ],
+  [[2018, 4, 9],
+   "Introductory Sound Workshop",
+   "https://www.facebook.com/events/1106142452859553/",
+  ],
+  [[2018, 4, 15],
+   "Contra Dance + Annual Meeting",
+   "https://www.facebook.com/events/549715282076651/",
+  ],
+  [[2018, 5, 6],
+   "Contra Dance",
+   "https://www.facebook.com/events/574033382970439/",
+  ],
+  [[2018, 5, 16],
+   "Pre-Open Band Jam",
+   "https://www.facebook.com/events/396232334176796/",
+  ],
+  [[2018, 5, 20],
+   "Contra Dance + Open Band",
+   "https://www.facebook.com/events/620538981614020/",
+  ],
+  [[2018, 6, 3],
+   "Contra Dance",
+   "https://www.facebook.com/events/237170377052016/",
+  ],
+  [[2018, 6, 17],
+   "Contra Dance + Family Dance",
+   "https://www.facebook.com/events/197904014181224/",
+  ],
+  [[2018, 6, 22],
+   "Sparkle",
+   "https://www.facebook.com/events/167617197250098/",
+  ],
+  [[2018, 7, 1],
+   "Last Contra Dance until September",
+   "https://www.facebook.com/events/171184873570116/",
+  ],
+  [[2018, 9, 2],
+   "First Contra Dance of Fall",
+   "https://www.facebook.com/events/2144325189183937/",
+  ],
+  [[2018, 9, 16],
+   "Contra Dance + Family Dance",
+   "https://www.facebook.com/events/461154714404261/",
+  ],
+  [[2018, 9, 29],
+   "Sparkle",
+   "https://www.facebook.com/events/486209035188271/",
+  ],
+  [[2018, 10, 7],
+   "Contra Dance",
+   "https://www.facebook.com/events/2299154823680259/",
+  ],
+  [[2018, 10, 19],
+   "Open Bandleading Party",
+   "https://www.facebook.com/events/438391453353775/",
+  ],
+  [[2018, 10, 21],
+   "Contra Dance + Open Band",
+   "https://www.facebook.com/events/2161173014208463/",
+  ],
+  [[2018, 10, 26],
+   "Spark in the Dark",
+   "https://www.facebook.com/events/1989692144440783/",
+  ],
+  [[2018, 11, 4],
+   "Contra Dance + Family Dance",
+   "https://www.facebook.com/events/326732104796681/",
+  ],
+  [[2018, 11, 18],
+   "Contra Dance",
+   "https://www.facebook.com/events/176641616620025/",
+  ],
+  [[2018, 12, 2],
+   "Contra Dance + Open Band",
+   "https://www.facebook.com/events/333991700763628/",
+  ],
+  [[2018, 12, 14],
+   "Spark in the Dark",
+   "https://www.facebook.com/events/277254516259723/",
+  ],
+  [[2018, 12, 16],
+   "Contra Dance",
+   "https://www.facebook.com/events/291062938411154/",
+  ],
+  [[2019, 1, 6],
+   "Contra Dance",
+   "https://www.facebook.com/events/226105528314855/",
+  ],
+  [[2019, 1, 20],
+   "Cancelled: Contra Dance + Family Dance",
+   "https://www.facebook.com/events/940230436169392/",
+  ],
+  [[2019, 2, 3],
+   "10th Anniversary Contra Dance",
+   "https://www.facebook.com/events/223886658537789/",
+  ],
+  [[2019, 2, 17],
+   "Contra Dance",
+   "https://www.facebook.com/events/352836972112394/",
+  ],
+  [[2019, 3, 3],
+   "Contra Dance",
+   "https://www.facebook.com/events/789992681362941/",
+  ],
+  [[2019, 3, 17],
+   "Contra Dance",
+   "https://www.facebook.com/events/309714253022042/",
+  ],
+  [[2019, 3, 22],
+   "Contra Dance Weekend: Beantown Stomp",
+   "https://www.facebook.com/events/2170366253251724/",
+  ],
+  [[2019, 3, 29],
+   "Introductory Sound Workshop",
+   "https://www.facebook.com/events/390410001514090/",
+  ],
+  [[2019, 4, 1],
+   "Introductory Sound Workshop",
+   "https://www.facebook.com/events/390410001514090/",
+  ],
+  [[2019, 4, 7],
+   "Contra Dance + Family Dance",
+   "https://www.facebook.com/events/1321464138001152/"
+  ],
+  [[2019, 4, 19],
+   "Sparkle (Harvard Square)",
+   "https://www.facebook.com/events/344495839504202/"
+  ],
+  [[2019, 4, 21],
+   "Contra Dance + Annual Meeting",
+   "https://www.facebook.com/events/2271306433085893/"
+  ],
+  [[2019, 5, 5],
+   "Contra Dance",
+   "https://www.facebook.com/events/340571863237617/",
+  ],
+  [[2019, 5, 19],
+   "Contra Dance + Open Band",
+   "https://www.facebook.com/events/520312035168646/"
+  ],
+  [[2019, 6, 2],
+   "Contra Dance",
+   "https://www.facebook.com/events/371811596771389/"
+  ],
+  [[2019, 6, 16],
+   "Contra Dance + Family Dance",
+   "https://www.facebook.com/events/415538668998430/"
+  ],
+  [[2019, 7, 7],
+   "Last Contra Dance of the Season",
+   "https://www.facebook.com/events/2295383910530999/"
+  ],
+  [[2019, 9, 1],
+   "First Contra Dance of the Season",
+   "https://www.facebook.com/events/3489870864371713/"
+  ],
+  [[2019, 9, 15],
+   "Contra + Family Dance + Open Band",
+   "https://www.facebook.com/events/486403805251034/",
+  ],
+  [[2019, 9, 29],
+   "Spark in the Dark",
+   "https://www.facebook.com/events/375177783410452/",
+  ],
+  [[2019, 10, 6],
+   "Contra + Advanced Dance",
+   "https://www.facebook.com/events/2394062140916848/",
+  ],
+  [[2019, 10, 20],
+   "Contra + Family Dance",
+   "https://www.facebook.com/events/2275068329271838/"
+  ],
+  [[2019, 11, 3],
+   "Contra Dance",
+   "https://www.facebook.com/events/510653343121672/"
+  ],
+  [[2019, 11, 17],
+   "Contra Dance"
+  ],
+  [[2019, 12, 1],
+   "Contra Dance + Open Band",
+   "https://www.facebook.com/events/2661022750624285/"
+  ],
+  [[2019, 12, 15],
+   "Contra Dance",
+   "https://www.facebook.com/events/2510781078977234/"
+  ],
+  [[2020, 1, 5],
+   "Contra Dance",
+   "https://www.facebook.com/events/586946245417632/"
+  ],
+  [[2020, 1, 13],
+   "Jamming Party",
+   "https://www.facebook.com/events/1016912412004342/"
+  ],
+  [[2020, 1, 19],
+   "Contra Dance + Scottish Ceilidh + Family Dance",
+   "https://www.facebook.com/events/2374783312742180/"
+  ],
+  [[2020, 2, 2],
+   "Contra Dance",
+   "https://www.facebook.com/events/595205404610424/",
+  ],
+  [[2020, 2, 16],
+   "Contra Dance",
+   "https://www.facebook.com/events/468964767121011/",
+  ],
+  [[2020, 3, 1],
+   "Contra Dance + Open Band",
+   "https://www.facebook.com/events/2491609797773348/",
+  ],
+  [[2020, 3, 15],
+   "CANCELLED: Contra Dance",
+   "https://www.facebook.com/events/172932543914445/",
+  ],
+  [[2020, 3, 16],
+   "CANCELLED: Jamming Party",
+   "https://www.facebook.com/events/1533057363536403/",
+  ],
+  [[2020, 3, 20],
+   "CANCELLED: Contra Dance Weekend: Beantown Stomp",
+   "https://www.beantownstomp.com"
+  ],
+  [[2020, 4, 5],
+   "CANCELLED: Contra Dance + Family Dance",
+  ],
+  [[2021, 1, 29],
+   "Online Jam",
+   "https://www.facebook.com/events/236920641372232/"
+   ],
+  [[2022, 2, 20],
+   "Contra Dance, mask and booster required",
+   "https://www.facebook.com/events/3041007029484801/"
+  ],
+  [[2022, 3, 6],
+   "Contra Dance, mask and booster required",
+   "https://www.facebook.com/events/1166550917082275/",
+  ],
+  [[2022, 3, 20],
+   "Contra Dance, mask and booster required",
+   "https://www.facebook.com/events/316871777091759/",
+  ],
+  [[2022, 4, 3],
+   "Contra Dance, mask and booster required",
+   "https://www.facebook.com/events/1662515587469007",
+  ],
+  [[2022, 4, 17],
+   "Contra Dance, mask and booster required",
+   "https://www.facebook.com/events/1377776709395062/",
+  ],
+  [[2022, 5, 1],
+   "Contra Dance, mask and booster required",
+   "https://www.facebook.com/events/758001815584715/",
+  ],
+  [[2022, 5, 15],
+   "Contra Dance + Open Band, mask and booster required",
+  ],
+  [[2022, 6, 5],
+   "Contra Dance, mask and booster required",
+  ],
+  [[2022, 6, 19],
+   "Contra Dance, mask and booster required",
+  ],
+  [[2022, 7, 3],
+   "Last Contra Dance of the Season, mask and booster required",
+  ],
+];
+
+events = document.getElementById("events");
+past_events = document.getElementById("past-events");
+
+function getHostname(url) {
+  var hostname = url.split('/')[2];
+  if (hostname.slice(0,4) == "www.") {
+    hostname = hostname.slice(4);
+  }
+
+  if (hostname == "facebook.com") {
+    return "facebook";
+  } else if (hostname == "meetup.com") {
+    return "meetup";
+  } else {
+    return hostname;
+  }
+}
+
+function gen_events() {
+  var yesterday = new Date(); yesterday.setDate(yesterday.getDate() - 1);
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+              "Friday", "Saturday"];
+  for (var i = 0; i < e.length; i++) {
+    var event = e[i];
+    var event_date_struct = event[0];
+    var event_title = event[1];
+    var event_link = null;
+    if (event.length > 2) {
+      event_link = event[2];
+    }
+    var event_date = new Date(event_date_struct[0],
+                              event_date_struct[1] - 1,
+                              event_date_struct[2]);
+    if (event_link) {
+      var event_a = document.createElement("a");
+      event_a.href = event_link;
+    }
+    var event_div = document.createElement("div");
+    event_div.className = "event";
+    var date_div = document.createElement("div");
+    date_div.className = "date";
+    var date_text = days[event_date.getDay()] + " " +
+                    (event_date.getMonth() + 1) + "/" +
+                    event_date.getDate();
+    if (event_date.getYear() != new Date().getYear()) {
+      date_text += "/" + (event_date.getYear() + 1900);
+    }
+    date_div.textContent = date_text;
+    event_div.appendChild(date_div);
+    var details_div = document.createElement("div");
+    details_div.className = "details";
+    var title_div = document.createElement("div");
+    title_div.className = "title";
+    title_div.textContent = event_title;
+    details_div.appendChild(title_div);
+    if (event.length > 3) {
+      event_custom_html = event[3] + "<p>";
+      custom_html_div = document.createElement("div");
+      custom_html_div.innerHTML = event_custom_html;
+      details_div.appendChild(custom_html_div);
+    }
+    var link_span = document.createElement("span");
+    if (event_link) {
+      link_span.className = "link";
+      var link_domain = getHostname(event_link);
+      link_span.textContent ="details on " + link_domain;
+    } else {
+      link_span.textContent ="details TBA";
+    }
+    details_div.appendChild(link_span);
+    event_div.appendChild(details_div);
+    clear_div = document.createElement("div");
+    clear_div.className = "clear";
+    event_div.appendChild(clear_div);
+    var append_event = event_div;
+    if (event_link) {
+      event_a.appendChild(event_div);
+      append_event = event_a;
+    }
+
+    if (event_date < yesterday) {
+      if (past_events.children.length == 0) {
+        past_events.appendChild(append_event);
+      } else {
+        past_events.insertBefore(
+          append_event, past_events.firstChild);
+      }
+    } else {
+      events.appendChild(append_event);
+    }
+  }
+}
+gen_events();
+
+show_past = false;
+toggle_past = document.getElementById("toggle-past");
+
+function update_toggle_past() {
+  if (show_past) {
+    toggle_past.textContent = "Hide Past Events";
+  } else {
+    toggle_past.textContent = "Show Past Events";
+  }
+}
+
+toggle_past.onclick = function() {
+  show_past = !show_past;
+  past_events.style.display = show_past ? "block" : "none";
+  update_toggle_past();
+  return false;
+};
+
+update_toggle_past();
+
