a40b06aa53a135f6162d2c15d28030a45d05fb66
Jeff Kaufman <jeff.t.kaufman@gmail.com>
2023-05-24 14:01:41 +0000

    missing comma

--- dances.js
+++ dances.js
@@ -733,7 +733,7 @@ e = [
   {
     "date": [2023, 6, 4],
     "band": "Rose Jackson, Rebecca Weiss, & Amy Englesberg",
-    "caller": "Chloe Levine"
+    "caller": "Chloe Levine",
     "link": ["https://www.meetup.com/bida-contra-dance/events/293723280/", "https://www.facebook.com/events/1388514941881652", "https://www.instagram.com/p/CsmfFUarFm_/"]
   },
   {
